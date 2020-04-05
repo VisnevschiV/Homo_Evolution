400135085b9a389f9d925da230dd2e09f7b15536
Daniel <bogatu.daniel21@yahoo.com>
2020-04-05 19:06:39 +0300

    Update

---
+++ Assets/Custom 2D Colliders/Scripts/Script_Buttons/Buttons_Evolue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons_Evolue : MonoBehaviour
+{
+    public GameObject[] _buttons;
+
+    public void Press()
+    {
+        
+        foreach (var p in _buttons)
+        {
+            p.gameObject.SetActive(true);
+        }
+    }
+}
