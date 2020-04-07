69eb98789aecc1527a8e9a10f8d147a24bb1be1c
Daniel <bogatu.daniel21@yahoo.com>
2020-04-07 17:12:15 +0300

    Bears script

---
+++ Assets/Custom 2 DColliders/Scripts/Enemy/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] public GameObject[] spawnPoints;
+
+    [SerializeField] public GameObject Bear;
+
+    [SerializeField] public float timeBetwenSpawn;
+    void Start()
+    {
+        StartCoroutine(Spawners(timeBetwenSpawn));
+    }
+
+    // Update is called once per frame
+    IEnumerator Spawners(float timeBetwenSpawn)
+    {
+        GameObject random = spawnPoints[Random.Range(0, spawnPoints.Length - 1)];
+        Instantiate(Bear, random.transform.position, random.transform.rotation);
+
+        yield return new WaitForSeconds(timeBetwenSpawn);
+        StartCoroutine(Spawners(this.timeBetwenSpawn));
+        
+    }
+}
